e5c0af78151d7bb5c0ddd30ec3c3a20479f4fde6
martinbarker <martinbarker99@gmail.com>
2021-05-25 18:19:03 -0700

    get access_token

--- app.js
+++ app.js
@@ -3,18 +3,47 @@ const app = express();
 const path = require('path');
 const router = express.Router();
 
+app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname + '/public/views');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+//home page
 router.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/index.html'));
-  //__dirname : It will resolve to your project folder.
+  res.sendFile(path.join(__dirname+'/home.html'));
 });
 
-router.get('/about',function(req,res){
-  res.sendFile(path.join(__dirname+'/about.html'));
+//if you go to the /login route, redirect you towards spotify login
+router.get('/login', function(req, res) {
+  var scopes = 'user-read-playback-position user-read-currently-playing user-modify-playback-state user-read-playback-state streaming app-remote-control user-library-modify user-library-read playlist-modify-public playlist-modify-private playlist-read-private playlist-read-collaborative';
+  var my_client_id = '199c96b7d70f4dd28f188f9c6bc86045';
+  var redirect_uri = 'http://localhost:3000/callback';
+  
+  res.redirect('https://accounts.spotify.com/authorize' +
+    '?response_type=code' +
+    '&client_id=' + my_client_id +
+    (scopes ? '&scope=' + encodeURIComponent(scopes) : '') +
+    '&redirect_uri=' + encodeURIComponent(redirect_uri)
+  );
 });
 
-router.get('/sitemap',function(req,res){
-  res.sendFile(path.join(__dirname+'/sitemap.html'));
-});
+//if route begins with /callback
+router.get(/^\/callback(.*)/, async function (req, res) {
+  var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+  console.log('fullUrl = ', fullUrl)
+
+  //get access_token from url
+  let spotifyAuthCode = '';
+  if(fullUrl.includes('?code=')){
+    spotifyAuthCode = fullUrl.substr(fullUrl.lastIndexOf('?code=')+7);
+    console.log('spotifyAuthCode=',spotifyAuthCode);
+  }
+
+    res.render('spotify.html', {
+      spotifyAuthCode:'aaa'
+    });
+  
+})
 
 //add the router
 app.use('/', router);
